7afc31313273bfaf89962c18fe5cbc73aff2f309
Daryl Sofia Gialolo <darylsg@uuzu.com>
2022-03-29 20:05:14 +0800

    Initial Commit

---
+++ Instragram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instragram
+//
+//  Created by Wind Versi on 27/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ProfileView(title: "netflix", imageName: "netflixLogo")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instragram/InstragramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstragramApp.swift
+//  Instragram
+//
+//  Created by Wind Versi on 27/3/22.
+//
+
+import SwiftUI
+
+@main
+struct InstragramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
